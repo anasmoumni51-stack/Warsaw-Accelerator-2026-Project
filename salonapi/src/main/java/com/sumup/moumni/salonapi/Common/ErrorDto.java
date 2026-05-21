a65d6bb18f2cabf033daf96fd454535d1fc66d5f
Anas Moumni <anas.moumni51@gmail.com>
2026-05-21 17:12:00 +0200

    salonapi: add global exception handling & custom exceptions

---
+++ salonapi/src/main/java/com/sumup/moumni/salonapi/Common/ErrorDto.java
@@ -0,0 +1,10 @@
+package com.sumup.moumni.salonapi.Common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ErrorDto {
+    private String error;
+}
