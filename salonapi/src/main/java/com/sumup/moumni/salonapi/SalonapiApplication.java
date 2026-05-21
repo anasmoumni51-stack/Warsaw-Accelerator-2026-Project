78ee38bd50f3a1ef5f739f5a3b41e085297a1ac2
Anas Moumni <anas.moumni51@gmail.com>
2026-05-21 15:18:00 +0200

    salonapi: configure H2 database & application entry point

---
+++ salonapi/src/main/java/com/sumup/moumni/salonapi/SalonapiApplication.java
@@ -0,0 +1,13 @@
+package com.sumup.moumni.salonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonapiApplication.class, args);
+    }
+
+}
