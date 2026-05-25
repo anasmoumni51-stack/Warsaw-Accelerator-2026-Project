8559f5e40f59722319b048c185087698642232cb
Anas Moumni <anas.moumni51@gmail.com>
2026-05-25 04:06:31 +0200

    salonapi: optimize queries with @EntityGraph for eager loading

--- salonapi/src/main/java/com/sumup/moumni/salonapi/Repository/SalonRepository.java
+++ salonapi/src/main/java/com/sumup/moumni/salonapi/Repository/SalonRepository.java
@@ -3,15 +3,23 @@ package com.sumup.moumni.salonapi.Repository;
 import com.sumup.moumni.salonapi.Entity.Salon;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SalonRepository extends JpaRepository<Salon, Long> {
 
+    @EntityGraph(attributePaths = "services")
     Page<Salon> findByDistrict(String district, Pageable pageable);
 
+    @EntityGraph(attributePaths = "services")
     Page<Salon> findByServicesName(String serviceName, Pageable pageable);
 
+    @EntityGraph(attributePaths = "services")
     Page<Salon> findByDistrictAndServicesName(String district, String serviceName, Pageable pageable);
+
+    @EntityGraph(attributePaths = "services")
+    @Override
+    Page<Salon> findAll( Pageable pageable);
 }
